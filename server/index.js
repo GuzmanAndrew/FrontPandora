2fba36b0391377c5825367d979de355c123fa169
DESKTOP-311EKJF\andrew <ramirezandrew172@gmail.com>
2022-10-10 16:57:22 -0500

    Final connnect arduino

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+var io = require('socket.io')(server);
+server.listen(process.env.PORT || 3000);
+console.log('Server is running');
+
+io.on('connection', function (socket) {
+   console.log('a new socket connection');
+})
+
+// routes
+app.get('/', (req, res) => {
+   res.sendFile(__dirname + '/dashboard.html');
+});
+
+// static files
+// app.use(express.static(path.join(__dirname, 'public')));
+
+const SerialPort = require('serialport').SerialPort;
+// const Readline = require('@serialport/parser-readline')
+
+const mySerial = new SerialPort({ path: 'COM13', baudRate: 9600 })
+
+/* const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+
+mySerial.pipe(parser); */
+
+mySerial.on('open', function () {
+  console.log('Opened Port.');
+});
+
+mySerial.on('data', function (data) {
+  // console.log(parseInt(data));
+  console.log(data.toString());
+  io.emit('arduino:data', {
+    value: data.toString()
+  });
+});
+
+mySerial.on('err', function (data) {
+  console.log(err.message);
+});
